4d1e130872ec267b7a3f94c303031f5997eb0377
zhaojinm <joshua@Joshuas-MacBook-Pro.local>
2018-11-17 16:48:24 -0500

    fix infi loop in select

--- select.cc
+++ select.cc
@@ -34,32 +34,71 @@ int main(int argc, char **argv){
 
     ftime(&t);
     now_in_ms1 = t.time*1000 + t.millitm;
-    while(cur_page!=(last_dir_page+numberofentry)){
-    	if (cur_page%(numberofentry+1)==0){
-    		fseek(heap_file->file_ptr, cur_page*page_size, SEEK_SET);
-    		fread(&last_dir_page,sizeof(int),1,heap_file->file_ptr);
-    		cur_page+=1;
-    	}
-    	read_page(heap_file,cur_page,page);
-    	int free_slot = fixed_len_page_freeslots(page);
-    	if (free_slot!=fixed_len_page_capacity(page)) total_pages+=1;
-    	for (int i = 0; i<fixed_len_page_capacity(page);i++){
-    		Record record;
-    		read_fixed_len_page(page,i,&record);
-
-    		if (record.size()>0 && record[0][0]!='0'){
-                if (strncmp(record[aid],argv[3],10)>=0 and strncmp(record[aid],argv[4],10)<=0){
-                    if(stdoutFlag){
-                        std::string stoprint = record[aid];
-                        printf("%s\n", stoprint.substr(0,5).c_str());       
+    while(cur_page<=(last_dir_page+numberofentry)){
+        int entoff = cur_page;
+        if (cur_page%(numberofentry+1)==0){
+            fseek(heap_file->file_ptr, cur_page*page_size, SEEK_SET);
+            entoff = cur_page*page_size+8;
+            fread(&last_dir_page,sizeof(int),1,heap_file->file_ptr);
+            cur_page+=1;
+        }
+        int pageoff,freespace;
+        while(cur_page%(numberofentry+1)!=0){
+            fseek(heap_file->file_ptr,entoff,SEEK_SET);
+            fread(&pageoff,sizeof(int),1,heap_file->file_ptr);
+            fread(&freespace,sizeof(int),1,heap_file->file_ptr);
+            if (freespace<page_size){
+                read_page(heap_file,pageoff,page);
+                total_pages+=1;
+                for (int i = 0; i<fixed_len_page_capacity(page);i++){
+                    Record record;
+                    read_fixed_len_page(page,i,&record);
+                    // printf("%d\n", record.size());
+                    if (record.size()>0 && record[0][0]!='0'){
+                         if (strncmp(record[aid],argv[3],10)>=0 and strncmp(record[aid],argv[4],10)<=0){
+                            if(stdoutFlag){
+                                std::string stoprint = record[aid];
+                                printf("%s\n", stoprint.substr(0,5).c_str());       
+                            }
+                        total_records++;
+                        }
                     }
-                    total_records++;
                 }
-	    	}
-    	}
-    	cur_page+=1;
-    	init_fixed_len_page(page, page_size, record_size);
+            }
+            cur_page+=1;
+            entoff+=8;
+        }
+        init_fixed_len_page(page, page_size, record_size);
     }
+
+
+    // while(cur_page!=(last_dir_page+numberofentry)){
+    // 	if (cur_page%(numberofentry+1)==0){
+    // 		fseek(heap_file->file_ptr, cur_page*page_size, SEEK_SET);
+    // 		fread(&last_dir_page,sizeof(int),1,heap_file->file_ptr);
+    // 		cur_page+=1;
+    // 	}
+    // 	read_page(heap_file,cur_page,page);
+    // 	int free_slot = fixed_len_page_freeslots(page);
+    // 	if (free_slot!=fixed_len_page_capacity(page)) total_pages+=1;
+    //     else break;
+    // 	for (int i = 0; i<fixed_len_page_capacity(page);i++){
+    // 		Record record;
+    // 		read_fixed_len_page(page,i,&record);
+
+    // 		if (record.size()>0 && record[0][0]!='0'){
+    //             if (strncmp(record[aid],argv[3],10)>=0 and strncmp(record[aid],argv[4],10)<=0){
+    //                 if(stdoutFlag){
+    //                     std::string stoprint = record[aid];
+    //                     printf("%s\n", stoprint.substr(0,5).c_str());       
+    //                 }
+    //                 total_records++;
+    //             }
+	   //  	}
+    // 	}
+    // 	cur_page+=1;
+    // 	init_fixed_len_page(page, page_size, record_size);
+    // }
 
     ftime(&t);
 	now_in_ms2 = t.time*1000 + t.millitm;
